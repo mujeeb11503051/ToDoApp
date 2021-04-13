f6fd96d00cf9a33be9d6d93703fb5efecf44a9ef
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-04-13 18:54:21 +0200

    added input to the list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React, {useState} from 'react'
 import './App.css';
 //component Imports
 import Form from './Components/Form';
 import ToDoList from './Components/ToDoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>Mujeeb's Todo List</h1>
       </header>
-      <Form/>
+      <Form todos = {todos} setTodos = {setTodos} setInputText = {setInputText} inputText = {inputText}/>
       <ToDoList/>
     </div>
   );
