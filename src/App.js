ddd93bf4cda396f2e1c2ab255d6d2f675ed18b88
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-04-13 12:27:51 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Mujeeb's Todo List........</h1>
+        <h1>Mujeeb's Todo List</h1>
       </header>
       <Form/>
       <ToDoList/>
